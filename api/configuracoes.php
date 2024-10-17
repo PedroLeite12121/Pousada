cb82f9c4f41a03a4374121e27e75c74e6fddb711
PedroLeite210901 <pedreirapedro40@gmail.com>
2024-10-16 21:14:14 -0300

    Update configuracoes.php

--- api/configuracoes.php
+++ api/configuracoes.php
@@ -8,7 +8,7 @@
     //variaveis do banco de dados
     $servername = getenv('DB_HOST');   
     $username = getenv('DB_USER');   
-    $password = getenv('DB_PASSWORD');  
+    $password = getenv('DB_PASS');  
     $dbname = getenv('DB_NAME');
     //inicia o banco
     $conn = new mysqli($servername, $username, $password, $dbname);
